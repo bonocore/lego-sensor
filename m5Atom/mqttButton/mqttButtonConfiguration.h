7f0f7de2bb0b12bc2639867d3d01c1256d7152f6
pi <giuseppe.bonocore@gmail.com>
2023-08-03 17:10:02 +0200

    First commit

---
+++ m5Atom/mqttButton/mqttButtonConfiguration.h
@@ -0,0 +1,7 @@
+#define SECRET_SSID  "GB_PVT_AP"
+#define SECRET_PASSWORD  "salesforce2023WTl3g0!"
+#define SECRET_MQTT_SERVER  "192.168.69.1"
+#define SECRET_MQTT_PORT  1883
+#define SECRET_MQTT_USER  "mqtt"
+#define SECRET_MQTT_PASSWORD  "salesforce1"
+#define SECRET_MQTT_TOPIC  "sensors/order"
---
+++ nodeMCU/RFID_MQTT/RFID_MQTT_Configuration.h
@@ -0,0 +1,9 @@
+#define WIFI_SSID "GB_PVT_AP"
+#define WIFI_PASSWORD "salesforce2023WTl3g0!"
+
+#define MQTT_HOST IPAddress(192, 168, 69, 1)
+#define MQTT_PORT 1883
+#define MQTT_USER "mqtt"
+#define MQTT_PASSWORD "salesforce1"
+
+#define MQTT_PUB_RFID "sensors/delivery"
